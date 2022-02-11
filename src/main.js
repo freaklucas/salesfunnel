aae8c82a41b8a6c887be5b892333f09d59a4ee27
freaklucas <lucas.oliveira.freak@gmail.com>
2022-02-11 20:30:59 -0300

    add router and components

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import BootstrapVue from "bootstrap-vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  BootstrapVue,
+  render: (h) => h(App),
+}).$mount("#app");

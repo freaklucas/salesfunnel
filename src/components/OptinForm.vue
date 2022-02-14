99826e018a007a109d4f68ce8441134c0a3a3558
freaklucas <lucas.oliveira.freak@gmail.com>
2022-02-14 10:49:58 -0300

    componente form

---
+++ src/components/OptinForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <div clas="optin-form">
+    <form @submit.prevent="onSubmit">
+      <b-form-group>
+        <b-form-input
+          type="email"
+          v-model="form.email"
+          size="lg"
+          required
+          placeholder="Insira email"
+        />
+        
+      </b-form-group>
+      <b-button type="submit" :variant="submitColor" size="lg" block>
+        {{ submitText }}
+      </b-button>
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "OptinForm",
+  props: {
+    submitText: String,
+    submitColor: String,
+  },
+  data() {
+    return {
+      form: {
+        email: ""
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$emit("submit", this.form);
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+  .btn {
+    font-weight: bold;
+    font-size: 1.5rem;
+  }
+</style>

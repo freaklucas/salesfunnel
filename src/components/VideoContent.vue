88ccf50c325d5beedc92cd6f601e68539a8e18b3
freaklucas <lucas.oliveira.freak@gmail.com>
2022-02-14 11:28:59 -0300

    component video

---
+++ src/components/VideoContent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="vide-content" :style="{ marginTop: topMargin }">
+    <b-embed type="iframe" aspect="16by9" :src="link" allowfullscreen />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VideoContent",
+  props: {
+    link: String,
+    topMargin: String,
+  },
+};
+</script>
